d0e918072d49dcb85e29e767eccfaad28f9023aa
Prapya <dev.prapya@gmail.com>
2026-01-13 11:29:23 -0500

    auth middleware optimized

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,66 +1,8 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { postController } from "./post.controller";
-import { auth as betterAuth } from "../../lib/auth";
+import auth, { userRole } from "../../middlewares/auth";
 
 const postRouter = express.Router();
-
-export enum userRole {
-    USER = "USER",
-    ADMIN = "ADMIN"
-}
-
-declare global {
-    namespace Express {
-        interface Request {
-            user?: {
-                id: string,
-                email: string,
-                name: string,
-                role: string,
-                emailVerified: boolean
-            }
-        }
-    }
-}
-
-const auth = (...roles: userRole[]) => async (req: Request, res: Response, next: NextFunction) => {
-    const session = await betterAuth.api.getSession({
-        headers: req.headers as any
-    });
-
-    if (!session) {
-        return res.status(401).json({
-            success: false,
-            message: "You are not authorized!"
-        });
-    }
-
-    if (!session.user.emailVerified) {
-        return res.status(403).json({
-            success: false,
-            message: "Email verification required. Please verify your email!"
-        });
-    }
-
-    req.user = {
-        id: session.user.id,
-        email: session.user.email,
-        name: session.user.name,
-        role: session.user.role as string,
-        emailVerified: session.user.emailVerified
-    };
-    console.log(session.user);
-
-    // console.log(req.user.role);
-    if (roles.length && !roles.includes(req.user?.role as userRole)) {
-        return res.status(403).json({
-            success: false,
-            message: "Forbidden, You have don't have permission to access!"
-        });
-    }
-
-    next();
-}
 
 postRouter.post("/", auth(userRole.USER), postController.createPost);
 postRouter.get("/", postController.getAllPosts);
