e71e978aa0826c98b8b0341b9a8ed723346ae84d
Prapya <dev.prapya@gmail.com>
2026-01-13 11:30:19 -0500

    auth middleware optimized again

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -21,42 +21,44 @@ declare global {
 }
 
 const auth = (...roles: userRole[]) => async (req: Request, res: Response, next: NextFunction) => {
-    const session = await betterAuth.api.getSession({
-        headers: req.headers as any
-    });
-
-    if (!session) {
-        return res.status(401).json({
-            success: false,
-            message: "You are not authorized!"
+    try {
+        const session = await betterAuth.api.getSession({
+            headers: req.headers as any
         });
+
+        if (!session) {
+            return res.status(401).json({
+                success: false,
+                message: "You are not authorized!"
+            });
+        }
+
+        if (!session.user.emailVerified) {
+            return res.status(403).json({
+                success: false,
+                message: "Email verification required. Please verify your email!"
+            });
+        }
+
+        req.user = {
+            id: session.user.id,
+            email: session.user.email,
+            name: session.user.name,
+            role: session.user.role as string,
+            emailVerified: session.user.emailVerified
+        };
+
+        if (roles.length && !roles.includes(req.user?.role as userRole)) {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden, You have don't have permission to access!"
+            });
+        }
+
+        next();
+    } catch (err) {
+        next(err);
     }
-
-    if (!session.user.emailVerified) {
-        return res.status(403).json({
-            success: false,
-            message: "Email verification required. Please verify your email!"
-        });
-    }
-
-    req.user = {
-        id: session.user.id,
-        email: session.user.email,
-        name: session.user.name,
-        role: session.user.role as string,
-        emailVerified: session.user.emailVerified
-    };
-    console.log(session.user);
-
-    // console.log(req.user.role);
-    if (roles.length && !roles.includes(req.user?.role as userRole)) {
-        return res.status(403).json({
-            success: false,
-            message: "Forbidden, You have don't have permission to access!"
-        });
-    }
-
-    next();
 }
 
 export default auth;
